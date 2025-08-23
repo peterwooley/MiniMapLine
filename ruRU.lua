d5d6802aae92e7cab539621558451b1e15ca18bf
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-08-23 11:05:06 +0300

    Create ruRU.lua

---
+++ ruRU.lua
@@ -0,0 +1,11 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MiniMapLine", "ruRU", true)
+-- Translator ZamestoTV
+L["Show Line"] = "Показать линию"
+L["Thickness"] = "Толщина"
+L["Opacity"] = "Прозрачность"
+L["Color"] = "Цвет"
+L["Color Heading"] = "Цвет линии"
+L["Shape"] = "Форма миникарты"
+L["Circle"] = "Круг"
+L["Square"] = "Квадрат"
+L["Shape description"] = "Выберите форму миникарты, и MiniMapLine адаптирует длину линии под выбранную форму"
